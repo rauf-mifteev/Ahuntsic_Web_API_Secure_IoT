112c0c4189d57d06c0b4306df3acc9c947c1218b
Rauf Mifteev <52508163+rauf-mifteev@users.noreply.github.com>
2026-06-03 00:25:23 -0400

    [Upd] Update app.js for Step 4
    Update app.js to add statistic dashboard

--- serveur/app.js
+++ serveur/app.js
@@ -1,11 +1,8 @@
 /* ============================================================
-   app.js — Point d'entrée du serveur (Projet final — Etape 2)
+   app.js — Point d'entrée du serveur (Projet final — Etape 4)
    ------------------------------------------------------------
-   Evolutions par rapport à l'étape 1 :
-     1. On crée un serveur HTTP explicite (http.createServer).
-     2. Socket.IO est branché sur ce serveur HTTP via socket.js.
-     3. L'instance io est exposée à Express via app.set("io", io).
-     4. Le serveur écoute sur serveurHttp (pas directement app).
+   Evolutions par rapport à l'étape 3 :
+     - Ajout de la route /api/statistiques (admin seulement).
    ============================================================ */
 
 const http    = require("http");
@@ -17,8 +14,9 @@ const { connecterBD }     = require("./config/bd");
 const { initialiser }     = require("./socket");
 const { middlewareErreur }= require("./controleurs/erreurs.middleware");
 
-const authRoutes    = require("./routes/auth.routes");
-const alertesRoutes = require("./routes/alertes.routes");
+const authRoutes         = require("./routes/auth.routes");
+const alertesRoutes      = require("./routes/alertes.routes");
+const statistiquesRoutes = require("./routes/statistiques.routes");
 
 
 const app = express();
@@ -31,7 +29,7 @@ const app = express();
 app.use(cors({
   origin: (origine, callback) => {
     //if (!origine) return callback(null, true);
-    if (!origine || origine === "null") return callback(null, true); // Insomnia / curl / file://
+    if (!origine || origine === "null") return callback(null, true);
     if (config.corsOrigines.includes(origine)) return callback(null, true);
     return callback(new Error(`Origine non autorisée : ${origine}`));
   },
@@ -53,8 +51,9 @@ app.use((req, res, next) => {
    2) Routes
    ------------------------------------------------------------ */
 
-app.use("/api/auth",    authRoutes);
-app.use("/api/alertes", alertesRoutes);
+app.use("/api/auth",         authRoutes);
+app.use("/api/alertes",      alertesRoutes);
+app.use("/api/statistiques", statistiquesRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Ressource introuvable." });
@@ -67,13 +66,8 @@ app.use(middlewareErreur);
    3) Serveur HTTP + Socket.IO
    ------------------------------------------------------------ */
 
-// On crée d'abord le serveur HTTP à partir de l'app Express,
-// puis on y branche Socket.IO. Les deux partagent le même port.
 const serveurHttp = http.createServer(app);
 const io          = initialiser(serveurHttp);
-
-// Mise à disposition de `io` pour les contrôleurs :
-//   req.app.get("io").emit("alerte:nouvelle", alerte)
 app.set("io", io);
 
 
